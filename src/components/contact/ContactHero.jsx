873aa27db0442f2682cf76bbb73ce520b74e5fe0
Saurabh Pandey <saurabhp643@gmail.com>
2025-04-10 14:25:32 +0530

    added contact

--- src/components/contact/ContactHero.jsx
+++ src/components/contact/ContactHero.jsx
@@ -4,7 +4,9 @@ import './ContactHero.css';
 const ContactHero = () => {
   return (
     <section className="about-hero">
+    
       <div className="about-overlay">
+      <img className="about" src="/vecteezy_contact-us-concept_1263984.jpg" alt="Image" />
         <h1>Contact US</h1>
         <p>Home &gt; Contact</p>
       </div>
